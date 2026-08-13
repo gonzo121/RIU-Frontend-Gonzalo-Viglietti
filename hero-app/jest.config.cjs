086dd9159e7c8d90bfa1e5f13966b64ddbcd5b54
gonzalo viglietti <gonzalo.viglietti@kyas.com.ar>
2026-08-13 18:31:21 -0300

    cargo configuracion jest coverage

--- hero-app/jest.config.cjs
+++ hero-app/jest.config.cjs
@@ -14,4 +14,15 @@ module.exports = {
     '!src/**/*.spec.ts',
     '!src/main.ts',
   ],
+
+  coverageDirectory: 'coverage',
+
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
