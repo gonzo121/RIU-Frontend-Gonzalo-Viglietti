00d9bcd420e9699368feaec1f14997b3a8cc94b4
gonzalo viglietti <gonzalo.viglietti@kyas.com.ar>
2026-08-13 21:12:44 -0300

    creacion de heroService con sus test, modelo y datos

---
+++ hero-app/src/app/features/heroes/data/hero.data.ts
@@ -0,0 +1,170 @@
+import { Hero } from '../models/hero.model';
+
+export const INITIAL_HEROES: Hero[] = [
+    {
+        id: 1,
+        name: 'Superman',
+        realName: 'Clark Kent',
+        franchise: 'DC Comics',
+        description: 'The Man of Steel',
+        superPower: 'Super strength and flight',
+        age: 35,
+        wearCape: true,
+        fromEarth: false
+    },
+    {
+        id: 2,
+        name: 'Batman',
+        realName: 'Bruce Wayne',
+        franchise: 'DC Comics',
+        description: 'The Dark Knight',
+        superPower: 'Genius intellect and martial arts skills',
+        age: 40,
+        wearCape: true,
+        fromEarth: true
+    }
+    ,
+    {
+        id: 3,
+        name: 'Wonder Woman',
+        realName: 'Diana Prince',
+        franchise: 'DC Comics',
+        description: 'Amazonian warrior princess',
+        superPower: 'Super strength and agility',
+        age: 3000,
+        wearCape: false,
+        fromEarth: false
+    },
+    {
+        id: 4,
+        name: 'Spider-Man',
+        realName: 'Peter Parker',
+        franchise: 'Marvel Comics',
+        description: 'Friendly neighborhood superhero',
+        superPower: 'Wall-crawling and spider-sense',
+        age: 18,
+        wearCape: false,
+        fromEarth: true
+    },
+    {
+        id: 5,
+        name: 'Iron Man',
+        realName: 'Tony Stark',
+        franchise: 'Marvel Comics',
+        description: 'Genius billionaire playboy philanthropist',
+        superPower: 'Powered armor suit',
+        age: 45,
+        wearCape: false,
+        fromEarth: true
+    },
+    {
+        id: 6,
+        name: 'Captain America',
+        realName: 'Steve Rogers',
+        franchise: 'Marvel Comics',
+        description: 'Super soldier and symbol of freedom',
+        superPower: 'Enhanced strength and agility',
+        age: 100,
+        wearCape: false,
+        fromEarth: true
+    },
+    {
+        id: 7,
+        name: 'Thor',
+        realName: 'Thor Odinson',
+        franchise: 'Marvel Comics',
+        description: 'God of Thunder',
+        superPower: 'Control over lightning and weather',
+        age: 1500,
+        wearCape: true,
+        fromEarth: false
+    },
+    {
+        id: 8,
+        name: 'Black Panther',
+        realName: 'T\'Challa',
+        franchise: 'Marvel Comics',
+        description: 'King of Wakanda and superhero',
+        superPower: 'Enhanced strength and agility, Vibranium suit',
+        age: 30,
+        wearCape: false,
+        fromEarth: true
+    },
+    {
+        id: 9,
+        name: 'Hulk',
+        realName: 'Bruce Banner',
+        franchise: 'Marvel Comics',
+        description: 'Green-skinned behemoth with incredible strength',
+        superPower: 'Superhuman strength and durability',
+        age: 40,
+        wearCape: false,
+        fromEarth: true
+    },
+    {
+        id: 10,
+        name: 'Flash',
+        realName: 'Barry Allen',
+        franchise: 'DC Comics',
+        description: 'Fastest man alive',
+        superPower: 'Super speed and time manipulation',
+        age: 28,
+        wearCape: false,
+        fromEarth: true
+    },
+    {
+        id: 11,
+        name: 'Green Lantern',
+        realName: 'Hal Jordan',
+        franchise: 'DC Comics',
+        description: 'Intergalactic peacekeeper with a power ring',
+        superPower: 'Power ring that can create constructs',
+        age: 32,
+        wearCape: false,
+        fromEarth: true
+    },
+    {
+        id: 12,
+        name: 'Aquaman',
+        realName: 'Arthur Curry',
+        franchise: 'DC Comics',
+        description: 'King of Atlantis and protector of the seas',
+        superPower: 'Ability to communicate with marine life and super strength',
+        age: 35,
+        wearCape: false,
+        fromEarth: true
+    },
+    {
+        id: 13,
+        name: 'Doctor Strange',
+        realName: 'Stephen Strange',
+        franchise: 'Marvel Comics',
+        description: 'Master of the mystic arts and Sorcerer Supreme',
+        superPower: 'Magic and manipulation of time and space',
+        age: 45,
+        wearCape: true,
+        fromEarth: true
+    },
+    {
+        id: 14,
+        name: 'Black Widow',
+        realName: 'Natasha Romanoff',
+        franchise: 'Marvel Comics',
+        description: 'Highly skilled spy and assassin',
+        superPower: 'Expert martial artist and marksman',
+        age: 35,
+        wearCape: false,
+        fromEarth: true
+    },
+    {
+        id: 15,
+        name: 'Wolverine',
+        realName: 'Logan',
+        franchise: 'Marvel Comics',
+        description: 'Mutant with regenerative healing factor and adamantium claws',
+        superPower: 'Regenerative healing and enhanced senses',
+        age: 150,
+        wearCape: false,
+        fromEarth: true
+    }
+];
